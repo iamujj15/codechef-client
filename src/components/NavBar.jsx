cf4d2bdba43772fcc736169314b1eff02f96921b
Ujjwal Upadhyay <90755156+iamujj15@users.noreply.github.com>
2023-12-24 23:45:47 +0530

    change routes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,11 +8,11 @@ const NavBar = () => {
         <>
             <Navbar bg="dark" data-bs-theme="dark" expand="lg" sticky="top" direction="horizontal" gap={6}>
                 <Container>
-                    <Navbar.Brand href="/">CCBlogs</Navbar.Brand>
+                    <Navbar.Brand href="/codechef-client/">CCBlogs</Navbar.Brand>
                     <Nav className="me-auto ms-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/about">About</Nav.Link>
-                        <Nav.Link href="/blogs">Blogs</Nav.Link>
+                        <Nav.Link href="/codechef-client/">Home</Nav.Link>
+                        <Nav.Link href="/codechef-client/about">About</Nav.Link>
+                        <Nav.Link href="/codechef-client/blogs">Blogs</Nav.Link>
                     </Nav>
                     <Nav>
                         <span>
